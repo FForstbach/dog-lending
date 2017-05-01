407878d7efac72a9fb727aa389a7001b6d0a43e2
Fredrik Forstbach <fredrik.forstbach@gmail.com>
2017-05-01 16:22:08 +0200

    added dog controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'dogs/index'
+
+  get 'dogs/new'
+
+  get 'dogs/create'
+
+  get 'dogs/edit'
+
+  get 'dogs/update'
+
+  get 'dogs/destroy'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
