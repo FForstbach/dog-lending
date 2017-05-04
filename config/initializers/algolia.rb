e1e2004325b87bed914e82b1465fe3b225e99d09
Sarah Willis <sarahbethwillis@gmail.com>
2017-05-04 17:32:57 +0200

    never push your api key

--- config/initializers/algolia.rb
+++ config/initializers/algolia.rb
@@ -1,1 +1,1 @@
-AlgoliaSearch.configuration = { application_id: "AURXVQ3BPJ", api_key: "f1b7b0a5f35378c74528b8ec3a9e889b" }
+AlgoliaSearch.configuration = { application_id: "AURXVQ3BPJ", api_key: ENV["ALGOLIA_KEY"] }
