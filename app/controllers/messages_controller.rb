056abf942cf395d30640ee0ee060a5fc803097a3
Fredrik Forstbach <fredrik.forstbach@gmail.com>
2017-05-04 18:42:01 +0200

    more changes but messages still not working

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -13,13 +13,13 @@ class MessagesController < ApplicationController
     @message.request = @request
 
     if @message.save respond_to do |format|
-      format.html { redirect_to request_path(@request) }
-      format.js # <-- will render `app/views/reviews/create.js.erb` end
-      end
+        format.html { redirect_to requests_path }
+        format.js # <-- will render `app/views/reviews/create.js.erb` end
+        end
     else
       respond_to do |format|
-      format.html { render 'requests/#{@request.id}' }
-      format.js # <-- idem end
+        format.html { render 'requests/#{@request.id}' }
+        format.js # <-- idem end
       end
     end
   end
