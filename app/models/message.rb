895298a39c09e259bbe2317690b89d6e9b598fbc
Fredrik Forstbach <fredrik.forstbach@gmail.com>
2017-05-08 18:16:56 +0200

    messages are being appended when created

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ApplicationRecord
+  validates :request, :presence => true
   belongs_to :request
   belongs_to :user
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,6 +1,6 @@
 class Request < ApplicationRecord
   belongs_to :dog
   belongs_to :user
-  has_many :messages
+  has_many :messages, :inverse_of => :request
   accepts_nested_attributes_for :messages
 end
