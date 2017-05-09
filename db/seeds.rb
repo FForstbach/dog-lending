26db26f05a1ad38912823e619d4a6b26dee5727e
Sarah Willis <sarahbethwillis@gmail.com>
2017-05-09 11:38:44 +0200

    added toys and snacks and fixed ids in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,27 +6,36 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Dog.destroy_all
-User.destroy_all
-
 
 seed_file = Rails.root.join('db', 'seeds', 'dogs.yml')
 dogs = YAML::load_file(seed_file)
 
-10.times do
-  u = User.new(email: Faker::Internet.free_email, password: "123456")
-  u.save!
+Dog.clear_index!
+
+
+if User.count == 0
+  puts "Creating users"
+  10.times do
+    u = User.new(email: Faker::Internet.free_email, password: "123456")
+    u.save!
+  end
+else
+  puts "Already have users"
 end
 
-dogs.each do |dog|
-  d = Dog.new(user_id: User.order("RANDOM()").first.id,
-    name: dog["name"],
-    age: rand(1..12),
-    size: dog["size"],
-    description: dog["description"],
-    breed: dog["breed"],
-    photo: File.open(File.join(Rails.root, "app/assets/images", dog["photo"])),
-    location: ["Hauptstr. ","Torstr. ","Kantstr.","Seestr.","Sonnenallee","Berliner Str. ", "Landsberger Allee "].sample + rand(1..80).to_s + ", Berlin" )
+puts "Creating #{dogs.count} dogs..."
+dogs.each_with_index do |dog, index|
+  d = Dog.find_or_create_by(id: index + 1)
+  d.user_id ||= User.order("RANDOM()").first.id
+  d.name = dog["name"]
+  d.age = rand(1..12)
+  d.size = dog["size"]
+  d.description = dog["description"]
+  d.breed = dog["breed"]
+  d.photo ||= File.open(File.join(Rails.root, "app/assets/images", dog["photo"]))
+  d.location = ["Hauptstr. ","Torstr. ","Kantstr. ","Seestr. ","Sonnenallee ","Berliner Str. ", "Landsberger Allee "].sample + rand(1..80).to_s + ", Berlin"
+  d.favorite_snack = ["Pizza crust", "Cheezburger," "Other dogs feces," "Filet Mignon", "Anything I can find", "Lasagne", "Currywurst", "an ice cube", "ice cream", "Meatballs", "Grass"].sample + rand(1..80).to_s
+  d.favorite_toy = ["Chewy Vuitton Bone", "Chewlululemon Mat", "Pugg Boot", "the family cat", "Diane von Furstinbone", "Tennis ball" ].sample + rand(1..80).to_s
   d.save!
 end
 
