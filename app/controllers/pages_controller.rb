85c86aa30b9bc4007496fc7172f8460189ea0a84
Sarah Willis <sarahbethwillis@gmail.com>
2017-05-11 12:41:30 +0200

    changed homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,17 @@ class PagesController < ApplicationController
     @dogs.each do |dog|
       @dog_locations += dog.latitude.to_s + "," + dog.longitude.to_s + "%7C"
     end
+
+    @markers = Gmaps4rails.build_markers(@dogs) do |dog, marker|
+      marker.lat dog.latitude
+      marker.lng dog.longitude
+
+      marker.picture({
+          :url => "http://chart.apis.google.com/chart?chst=d_map_pin_icon&chld=glyphish_dogpaw|96b976",
+          :width   => 32,
+          :height  => 32
+       })
+    end
   end
 
   def dashboard
